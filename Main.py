ed1928537fd8049d0dc2502fc6ecbe1ac1266e6c
jeppeandersson22 <jeppeandersson22@gmail.com>
2024-04-20 19:10:42 +0200

    added excel file as output

--- Main.py
+++ Main.py
@@ -14,14 +14,14 @@ print(cleanData)
 print(cleanData.dtypes)
 
 ### TIL EXCEL ###
-'''
+
 # Specify the file name and location on the desktop
 file_name = "car_data.xlsx"
 desktop_path = "/Users/jeppeandersson/Desktop"
 file_path = os.path.join(desktop_path, file_name)
 
 # Save the DataFrame to an Excel file
-result_df.to_excel(file_path, index=False)
+cleanData.to_excel(file_path, index=False)
 print(f"Excel file saved to: {file_path}")
-'''
+
 
