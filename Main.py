8dadaa0f2c71de1350ddb51775a2833177fa66cc
jeppeandersson22 <jeppeandersson22@gmail.com>
2024-04-24 18:17:05 +0200

    changed Main

--- Main.py
+++ Main.py
@@ -7,19 +7,9 @@ import pandas as pd
 
 
 dirtyData = scrapeCars()
-print(len(dirtyData))
 cleanData = cleaning(dirtyData)
 
 
-### TIL EXCEL ###
 
-# Specify the file name and location on the desktop
-file_name = "car_data.xlsx"
-desktop_path = "/Users/jeppeandersson/Desktop"
-file_path = os.path.join(desktop_path, file_name)
-
-# Save the DataFrame to an Excel file
-cleanData.to_excel(file_path, index=False)
-print(f"Excel file saved to: {file_path}")
 
 
