ecd0f5bf11bdbc4fcdf19ee7067688309571b161
jeppeandersson22 <jeppeandersson22@gmail.com>
2024-05-14 10:09:43 +0200

    Final changes

--- Clean.py
+++ Clean.py
@@ -6,49 +6,35 @@ dirtyData = pd.read_excel("dirty_car_data.xlsx")
 
 def cleaning(dirtyData):
     dirtyData = dirtyData.drop(columns=['Co2'])
-    total_nan_count = dirtyData.isna().sum()
-    print("Total number of NaN values in the DataFrame:", total_nan_count.sum())
 
     # Drop duplicates and rows with NaN as it is only a handful of rows so no need to impute
     dirtyData = dirtyData.drop_duplicates().dropna()
-    total_nan_count = dirtyData.isna().sum()
-    print("Step 1 Total number of NaN values in the DataFrame:", total_nan_count.sum())
 
     # Removing 'dage' from 'DageTilSalg' and converting to integer
     dirtyData['DageTilSalg'] = dirtyData['DageTilSalg'].str.extract('(\d+)').astype(int)
-    total_nan_count = dirtyData.isna().sum()
-    print("Step 2 Total number of NaN values in the DataFrame:", total_nan_count.sum())
+
 
     # Extracting the year from 'Årstal'
     dirtyData['Årstal'] = dirtyData['Årstal'].str.extract('(\d{4})').astype(int)
-    total_nan_count = dirtyData.isna().sum()
-    print("Step 3 Total number of NaN values in the DataFrame:", total_nan_count.sum())
     
     # Removing 'km' from 'Kilometertal' and converting to integer
     dirtyData['Kilometertal'] = dirtyData['Kilometertal'].str.replace('.', '').str.extract('(\d+)').astype(int)
-    total_nan_count = dirtyData.isna().sum()
-    print("Step 4 Total number of NaN values in the DataFrame:", total_nan_count.sum())
 
     # Extracting Hæstekræfter
     dirtyData['Hestekræfter'] = dirtyData['Hestekræfter'].str.extract('(\d+) HK')
-    total_nan_count = dirtyData.isna().sum()
-    print("Step 5 Total number of NaN values in the DataFrame:", total_nan_count.sum())
+
 
     # Replace 'El/Benzin' with 'Hybrid' in the 'Motor' column
     dirtyData['Brændstof'] = dirtyData['Motor'].str.replace('El/Benzin', 'Hybrid', regex=True)
-    total_nan_count = dirtyData.isna().sum()
-    print("Step 5 Total number of NaN values in the DataFrame:", total_nan_count.sum())
 
     # Splitting 'Motor' into 'Motorstørrelse' and 'Brændstof'
     dirtyData['Motorstørrelse'] = dirtyData['Motor'].str.extract('(\d+\.\d+|\d+)L').astype(float)
     dirtyData['Brændstof'] = dirtyData['Motor'].str.extract('(Diesel|Benzin|El|Hybrid)', flags=re.IGNORECASE)
-    total_nan_count = dirtyData.isna().sum()
-    print("Step 6 Total number of NaN values in the DataFrame:", total_nan_count.sum())
+    # Filter out rows where 'Brændstof' is 'El'
+    dirtyData = dirtyData[dirtyData['Brændstof'] != 'El']
 
     # Removing 'Km/L' from 'Km/L' and converting to float
     dirtyData['Km/L'] = dirtyData['Km/L'].str.replace(',', '.').str.extract('(\d+\.\d+)').astype(float)
-    total_nan_count = dirtyData.isna().sum()
-    print("Step 7 Total number of NaN values in the DataFrame:", total_nan_count.sum())
 
     # Removing 'kr.' and converting 'Pris' to float
     dirtyData['Pris'] = dirtyData['Pris'].str.replace('kr.', '').str.replace('.', '').astype(float)
